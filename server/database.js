c30f759806e6bf172aba82b832930b95ef36001c
Peter Oehman <tucker2923@gmail.com>
2022-01-10 16:10:04 -0500

    added authentication methods on user model

--- server/database.js
+++ server/database.js
@@ -1,4 +1,9 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const axios = require('axios')
 const Sequelize = require('sequelize')
+
+const SALT_ROUNDS = 5
 
 const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/bugTracker', {
   logging: false
@@ -26,5 +31,44 @@ module.exports = {
   db,
   models: {
     User
+  }
+}
+
+User.prototype.correctPassword = function(attemptedPwd) {
+  return jwt.compare(attemptedPwd, this.password)
+}
+
+User.prototype.generateToken = function() {
+  return jwt.sign({id: this.id}, process.env.JWT)
+}
+
+User.authenticate = async function({username, password}) {
+  const user = await this.findOne({where: username})
+  if (!user || !(await user.correctPassword(password))) {
+    const error = Error('Incorrect username/password')
+    error.status = 401;
+    throw error
+  }
+  return user.generateToken()
+}
+
+User.findByToken = async function(token) {
+  try {
+    const {id} = await jwt.verify(token, process.env.JWT)
+    const user = User.findBYPk(id)
+    if (!user) {
+      throw 'nope'
+    }
+    return user
+  } catch (err) {
+    const error = Error('bad token')
+    error.status = 401
+    throw error
+  }
+}
+
+const hashPassword = async(user) => {
+  if (user.changed('password')) {
+    user.password = await bcrypt.hash(user.password, SALT_ROUNDS)
   }
 }
--- server/seed.js
+++ server/seed.js
@@ -1,2 +1,25 @@
 const { db, models: {User}} = require('./database')
 
+async function createUser() {
+  await User.create({
+    username: 'Peter',
+    password: '123'
+  })
+}
+
+async function runSeed() {
+  console.log("seeding...");
+  try {
+    await db.sync({force: true})
+    await createUser()
+  } catch (err) {
+    console.error(err);
+    process.exitCode = 1;
+  } finally {
+    console.log("closing db connection");
+    await db.close();
+    console.log("db connection closed");
+  }
+}
+
+runSeed()
